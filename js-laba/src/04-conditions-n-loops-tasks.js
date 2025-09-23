291eb82d6f2ab3bf3a145b88279e3d749aca58f9
Strannik73 <Santon@tutamail.com>
2025-09-23 13:20:49 +0300

    kjre6tk

--- js-laba/src/04-conditions-n-loops-tasks.js
+++ js-laba/src/04-conditions-n-loops-tasks.js
@@ -26,9 +26,23 @@
  *  21 => 'Fizz'
  *
  */
-function getFizzBuzz(/* num */) {
-  throw new Error('Not implemented');
+function getFizzBuzz(num) {
+
+  if (num % 3 === 0  && num % 5 === 0){
+    return "FizzBuzz";
+  }   
+  if (num % 3 === 0) {
+    return "Fizz"; 
+  }
+  if (num % 5 === 0) {
+    return "Buzz"; 
+  }
+  else {
+    return num;
+  }
 }
+console.log(getFizzBuzz(2))
+// работает в терминале
 
 /**
  * Returns the factorial of the specified integer n.
@@ -41,9 +55,19 @@ function getFizzBuzz(/* num */) {
  *   5  => 120
  *   10 => 3628800
  */
-function getFactorial(/* n */) {
-  throw new Error('Not implemented');
+function getFactorial( n ) {
+  if (n < 0){
+    return 'неопределено';
+  }
+  if (n === 0 || n === 1){
+    return 1;
+  }
+  else{
+    return n * getFactorial(n - 1);
+  }
 }
+console.log(getFactorial(5));
+// работает в терминале
 
 /**
  * Returns the sum of integer numbers between n1 and n2 (inclusive).
@@ -57,9 +81,10 @@ function getFactorial(/* n */) {
  *   5,10  =>  45 ( = 5+6+7+8+9+10 )
  *   -1,1  =>  0  ( = -1 + 0 + 1 )
  */
-function getSumBetweenNumbers(/* n1, n2 */) {
-  throw new Error('Not implemented');
+function getSumBetweenNumbers(n1, n2 ) {
+  return  (n1 + n2) * (n2 - n1 + 1) / 2; 
 }
+console.log(getSumBetweenNumbers(1, 3))
 
 /**
  * Returns true, if a triangle can be built with the specified sides a, b, c
@@ -76,9 +101,16 @@ function getSumBetweenNumbers(/* n1, n2 */) {
  *   10,1,1   =>  false
  *   10,10,10 =>  true
  */
-function isTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isTriangle( a, b, c ) {
+  if (a<=0 || b<=0 || c<=0) {
+    return false;
+  }
+  if (a + b >= c && a + c >= b && c + b >= a ){
+    return true;
+  }
 }
+console.log(isTriangle(4, 9, 6))
+// работает в консоли
 
 /**
  * Returns true, if two specified axis-aligned rectangles overlap, otherwise false.
